590fd75a2d3e8c6c1bc9863c0e0333e8987e590c
RoboPesce <stlecorno@gmail.com>
2025-05-27 17:11:07 -0700

    testing screen FPS

--- picOS.c
+++ picOS.c
@@ -12,27 +12,13 @@ int main()
     mouse_position.vertical = NUM_ROWS / 2;
     mouse_position.horizontal = NUM_COLS / 2;
 
-    clear_framebuffer(0, 0, 0);
-
-    size_t shift = 0;
+    uint8_t color_shift = 0;
     
     while (true)
     {
         // Draw animation
 
-        for (size_t row = 0; row < NUM_ROWS; row++)
-        {
-            for (size_t col = 0; col < NUM_COLS; col++)
-            {
-                size_t shift_row = (row + shift) % NUM_ROWS;
-                size_t shift_col = (col + shift) % NUM_COLS;
-
-                uint8_t r = (shift_row * 255) / (NUM_ROWS - 1);
-                uint8_t g = (shift_col * 255) / (NUM_COLS - 1);
-                uint8_t b = ((shift_row + shift_col) * 255) / (NUM_ROWS + NUM_COLS - 2);
-                write_pixel_to_framebuffer(row, col, r, g, b);
-            }
-        }
+        clear_framebuffer(color_shift, color_shift + 100, color_shift + 200);
 
         // Move mouse
 
@@ -48,7 +34,7 @@ int main()
 
         draw_framebuffer();
 
-        shift += 5;
+        color_shift++;
     }
 
     /*
