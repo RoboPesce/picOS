2c98964fa579b211825f30490607da618a96fed2
Stefano Corno <stlecorno@gmail.com>
2025-09-17 18:14:20 -0700

    last touches

--- drivers/st7789_8080_driver.c
+++ drivers/st7789_8080_driver.c
@@ -5,6 +5,7 @@
 #include "st7789_8080_driver.h"
 #include "st7789_8080.pio.h"
 
+#define PIO_CLOCK_DIVIDER 3.0f
 #define POST_COMMAND_REST_MS 20 // Minimum: 5 ms. Raise in case of instability
 
 #define DATA_PIN_BASE 0 // Starting GPIO pin for data pins (GP0-GP7)
@@ -27,6 +28,9 @@ uint dma_chan;
 void write_command(uint8_t cmd);
 void write_data_single(uint8_t data);
 void write_data(const uint8_t *data, size_t len);
+
+static inline void st7789_8080_program_init(PIO pio, uint sm, uint offset,
+                                            uint data_pin_base, uint wr_pin);
 
 void st7789_8080_init() 
 {
@@ -120,4 +124,32 @@ void write_data(const uint8_t *data, size_t len)
 
     // Optionally: wait for completion
     dma_channel_wait_for_finish_blocking(dma_chan);
+}
+
+static inline void st7789_8080_program_init(PIO pio, uint sm, uint offset,
+                                            uint data_pin_base, uint wr_pin)
+{
+    // Configure 8 consecutive pins for data
+    pio_sm_set_consecutive_pindirs(pio, sm, data_pin_base, DATA_PIN_COUNT, true);
+
+    // Configure sideset pin (WR)
+    pio_sm_set_consecutive_pindirs(pio, sm, wr_pin, 1, true);
+
+    pio_sm_config c = st7789_8080_program_get_default_config(offset);
+
+    // Map "out pins" 8 data pins
+    sm_config_set_out_pins(&c, data_pin_base, 8);
+
+    // Map sideset pin
+    sm_config_set_sideset_pins(&c, wr_pin);
+
+    // Shift settings: push 8 bits from FIFO -> OSR at a time
+    sm_config_set_out_shift(&c, false, true, 8);
+
+    // Clock divider (controls PIO speed)
+    sm_config_set_clkdiv(&c, PIO_CLOCK_DIVIDER);
+
+    // Initialize SM with this config
+    pio_sm_init(pio, sm, offset, &c);
+    pio_sm_set_enabled(pio, sm, true);
 }
